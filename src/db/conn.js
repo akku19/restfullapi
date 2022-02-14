8c95c5fbd4b9def26ca215843e7530899b3a133f
akku19 <akashsen127@gmail.com>
2022-02-14 12:38:39 +0530

    add env

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
-console.log(process.env.DB);
+
 mongoose.connect(process.env.DB,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
